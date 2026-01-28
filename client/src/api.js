bb1c7368fe89a0fedc5bc831dd12339dafe19299
Santhu42 <santhiyagovindaraj42@gmail.com>
2026-01-29 01:49:59 +0530

    Update frontend API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ai-personal-finance-advisor-using-mern-usuh.onrender.com/",
 });
 
 export default api;
