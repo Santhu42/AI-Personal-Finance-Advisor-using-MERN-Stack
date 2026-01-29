e466200a57507daeb00d4eeceb6f8eb674f559a5
Santhu42 <santhiyagovindaraj42@gmail.com>
2026-01-29 10:15:21 +0530

    Fix CORS for Netlify frontend

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://vermillion-monstera-be6f4a.netlify.app/"
+      "https://vermillion-monstera-be6f4a.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
